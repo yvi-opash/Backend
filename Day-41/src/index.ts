4297759b5454df16d5d048ae79aea04ddbcd56d0
Yash <yvi.opash@gmail.com>
2026-02-16 13:46:46 +0530

    3

--- Day-41/src/index.ts
+++ Day-41/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("🚀 TypeScript Server Running...");
+  res.send(" TypeScript Server Running...");
 });
 
 app.listen(PORT, () => {
